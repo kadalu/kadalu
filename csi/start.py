b6d6c09435d9254c503c291e694368e440a95a8f
Amar Tumballi <amar@kadalu.io>
2021-02-09 20:39:55 +0530

    csi: start quota only in provisioner
    Credits: @aravindavk
    
    Signed-off-by: Amar Tumballi <amar@kadalu.io>

--- csi/start.py
+++ csi/start.py
@@ -10,7 +10,9 @@ def main():
     mon = Monitor()
     mon.add_process(Proc("csi", "python3", [curr_dir + "/main.py"]))
     mon.add_process(Proc("metrics", "python3", [curr_dir + "/exporter.py"]))
-    mon.add_process(Proc("quota", "bash", [curr_dir + "/quota-crawler.sh"]))
+
+    if os.environ.get("CSI_ROLE", "-") == "provisioner":
+        mon.add_process(Proc("quota", "bash", [curr_dir + "/quota-crawler.sh"]))
 
     mon.start_all()
     mon.monitor()
