192a217b4731ef6d8c13077650deaf729fd1b4e0
Leela Venkaiah G <lgangava@redhat.com>
2021-03-12 12:04:47 +0530

    Mount volume if exists before starting grpc service
    Fixes: https://github.com/kadalu/kadalu/issues/469
    
    Signed-off-by: Leela Venkaiah G <lgangava@redhat.com>

--- csi/main.py
+++ csi/main.py
@@ -1,21 +1,40 @@
 """
 Starting point of CSI driver GRP server
 """
-from concurrent import futures
-import time
 import logging
 import os
-
-import grpc
+import time
+from concurrent import futures
 
 import csi_pb2_grpc
-from identityserver import IdentityServer
+import grpc
 from controllerserver import ControllerServer
+from identityserver import IdentityServer
+from kadalulib import CommandException, logf, logging_setup
 from nodeserver import NodeServer
-from kadalulib import logging_setup
-
+from volumeutils import (HOSTVOL_MOUNTDIR, get_pv_hosting_volumes,
+                         mount_glusterfs)
 
 _ONE_DAY_IN_SECONDS = 60 * 60 * 24
+
+def mount_storage():
+    """
+    Mount storage if any volumes exist after a pod reboot
+    """
+    if os.environ.get("CSI_ROLE", "-") != "provisioner":
+        logging.debug("Volume need to be mounted on only provisioner pod")
+        return
+
+    host_volumes = get_pv_hosting_volumes({})
+    for volume in host_volumes:
+        hvol = volume['name']
+        mntdir = os.path.join(HOSTVOL_MOUNTDIR, hvol)
+        try:
+            mount_glusterfs(volume, mntdir)
+        except CommandException:
+            logging.error(logf("Unable to mount volume", hvol=hvol))
+        logging.info(logf("Volume is mounted successfully", hvol=hvol))
+    return
 
 
 def main():
@@ -24,6 +43,9 @@ def main():
     the GRPC server in required endpoint
     """
     logging_setup()
+
+    # If Provisioner pod reboots, mount volumes if they exist before reboot
+    mount_storage()
 
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     csi_pb2_grpc.add_ControllerServicer_to_server(ControllerServer(), server)
