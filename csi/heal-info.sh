24de612b043c48beff94ef34f55f9ca339c27910
hunter86bg <hunter86_bg@yahoo.com>
2021-11-06 15:08:54 +0200

    Add extended heal info (#690)
    Fixes: #684
    Signed-off-by: hunter86bg <hunter86_bg@yahoo.com>

--- csi/heal-info.sh
+++ csi/heal-info.sh
@@ -28,7 +28,19 @@ fi
 for volfile in $(cd /kadalu/volfiles; ls *); do
     #/opt/libexec/glusterfs/glfsheal <VOLNAME> [bigger-file <FILE> | latest-mtime <FILE> | source-brick <HOSTNAME:BRICKNAME> [<FILE>] | split-brain-info | info-summary] [glusterd-sock <FILE> | volfile-path <FILE>]
     vol=${volfile%.client.vol}
-    echo "Giving heal information of volume $vol"
-    /opt/libexec/glusterfs/glfsheal $vol info-summary volfile-path /kadalu/volfiles/$volfile
+    echo "Giving heal summary of volume ${vol}:"
+    # Storing the output of the info-summary for later evaluation
+    result=$(/opt/libexec/glusterfs/glfsheal $vol info-summary volfile-path /kadalu/volfiles/$volfile )
+    echo "$result"
     echo
+    # If info-summary has only "0" or "-" our exit code is "1" and we skip checking for heal details/split-brain
+    echo "$result" | grep "Total Number of entries" | cut -d ":" -f 2 |  grep -q -vE "0|-"
+    if [ "$?" -eq 0 ]; then
+            echo "List of files needing a heal on ${vol}:"
+            /opt/libexec/glusterfs/glfsheal $vol volfile-path /kadalu/volfiles/$volfile
+            echo
+            echo "List of files in splitbrain on ${vol}:"
+            /opt/libexec/glusterfs/glfsheal $vol split-brain-info volfile-path /kadalu/volfiles/$volfile
+    fi
+    echo;echo
 done
