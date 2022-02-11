1515f248c1ce9d3a01502daed4611b7be0b332fb
Shree Vatsa N <vatsa@kadalu.io>
2022-02-11 10:01:13 +0530

    cli: Add support to remove archived pvcs (#755)
    * remove archived pvs, info folder, validation
    * Use argparse for cmd line args
    
    Signed-off-by: Shree Vatsa N <vatsa@kadalu.io>

---
+++ cli/kubectl_kadalu/remove_archived_pv.py
@@ -0,0 +1,106 @@
+"""
+'remove-archived-pv' subcommand for kubectl-kadalu CLI tool
+"""
+from __future__ import print_function
+import sys
+import json
+import utils
+
+# noqa # pylint: disable=too-many-instance-attributes
+# noqa # pylint: disable=useless-object-inheritance
+# noqa # pylint: disable=too-few-public-methods
+# noqa # pylint: disable=bad-option-value
+def set_args(name, subparsers):
+    """ add arguments to argparser """
+    parser = subparsers.add_parser(name)
+    arg = parser.add_argument
+
+    arg("name", metavar="NAME",
+        help="Specify Storage name to delete archived pvc(s)")
+    arg("--pvc", metavar="PVC", default=None,
+        help="name of archived pvc belonging to specified storage-pool")
+    utils.add_global_flags(parser)
+
+
+def validate(args):
+    """
+    Validate the storage of archived pvcs requested to be delete
+    is present in kadalu configmap or not.
+    Exit if not present.
+    """
+
+    storage_info_data = get_configmap_data(args)
+
+    if storage_info_data is None:
+        print("Aborting.....")
+        print("Invalid name. No such storage '%s' in Kadalu configmap." % args.name)
+        sys.exit()
+
+
+def get_configmap_data(args):
+    """
+    Get storage info data from kadalu configmap
+    """
+
+    cmd = utils.kubectl_cmd(args) + [
+        "get", "configmap", "kadalu-info", "-nkadalu", "-ojson"
+    ]
+
+    try:
+        resp = utils.execute(cmd)
+        config_data = json.loads(resp.stdout)
+
+        volname = args.name
+        data = config_data['data']
+        storage_name = "%s.info" % volname
+        storage_info_data = data[storage_name]
+
+        # Return data in 'dict' format
+        return json.loads(storage_info_data)
+
+    except utils.CommandError as err:
+        utils.command_error(cmd, err.stderr)
+        return None
+
+    except KeyError:
+        # Validate method expects None when 'storage' not found.
+        return None
+
+
+def request_pv_delete(args):
+    """ Send PVC delete request to CSI"""
+
+    cmd = utils.kubectl_cmd(args) + [
+         "exec", "-it", "kadalu-csi-provisioner-0", "-c", "kadalu-provisioner", "-nkadalu",
+         "--", "bash",
+         "-c", "cd /kadalu; python3 remove_archived_pv.py %s" %(args.name)
+    ]
+
+    if args.pvc:
+        cmd[-1] = cmd[-1] + " --pvc=%s" %(args.pvc)
+
+    try:
+        resp = utils.execute(cmd)
+        print("Sent request for deletion of archived PVCs")
+        return resp
+
+    except utils.CommandError as err:
+        print("Failed to request deletion of archived pvc of the "
+                "storage \"%s\"" % args.name,
+                file=sys.stderr)
+        print(err, file=sys.stderr)
+        print()
+        return None
+    except FileNotFoundError:
+        utils.kubectl_cmd_help(args.kubectl_cmd)
+        return None
+
+
+def run(args):
+    """Run Delete Archived PVCs"""
+
+    resp = request_pv_delete(args)
+    if resp is not None:
+        if resp.stderr:
+            print(resp.stderr)
+        print(resp.stdout)
---
+++ csi/remove_archived_pv.py
@@ -0,0 +1,89 @@
+import sys
+import os
+import shutil
+import argparse
+from errno import ENOTCONN
+from volumeutils import HOSTVOL_MOUNTDIR, update_free_size, yield_pvc_from_mntdir
+from kadalulib import retry_errors
+
+
+def get_archived_pvs(storage_name, pvc_name):
+    """ Return all or specified archived_pvcs based on agrs """
+
+    archived_pvs = {}
+    mntdir = os.path.join(HOSTVOL_MOUNTDIR, storage_name)
+    try:
+        for pvc in yield_pvc_from_mntdir(mntdir):
+            if pvc is not None:
+
+                # With --pvc arg
+                if pvc_name is not None and pvc_name == pvc["name"]:
+                    archived_pvs[pvc["name"]] = pvc
+                    return archived_pvs
+
+                # Check for all archived pvcs
+                if pvc_name is None and "archived-" in pvc["name"]:
+                    archived_pvs[pvc["name"]] = pvc
+
+        # Return -1 if no matched specified pvc
+        if pvc_name is not None:
+            sys.stderr.write("Specified PVC %s is not found" % pvc_name)
+            return -1
+
+        # This return is for without --pvc.
+        return archived_pvs
+
+    except FileNotFoundError:
+        sys.stderr.write("Storage pool %s is not found" % storage_name)
+        return -1
+
+
+def delete_archived_pvs(storage_name, archived_pvs):
+    """ Delete all archived pvcs in archived_pvs """
+
+    for pvname, values in archived_pvs.items():
+
+        # Check for mount availablity before deleting info file & PVC
+        mntdir = os.path.join(HOSTVOL_MOUNTDIR, storage_name)
+        retry_errors(os.statvfs, [mntdir], [ENOTCONN])
+
+        # Remove PV in stat.db
+        update_free_size(storage_name, pvname.replace("archived-", ""), values["size"])
+
+        # Delete info file
+        info_file_path = os.path.join(mntdir, "info", values["path_prefix"], pvname + ".json")
+        shutil.rmtree(os.path.dirname(info_file_path))
+
+        # Delete PVC
+        pvc_path = os.path.join(mntdir, values["path_prefix"], pvname)
+        shutil.rmtree(os.path.dirname(pvc_path))
+
+
+def main():
+    """ main """
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("name", help="name of storage-pool")
+    parser.add_argument("--pvc", default=None,
+                        help="name of archived pvc belonging to specified storage-pool")
+
+    args = parser.parse_args()
+
+    if args.pvc and not args.pvc.startswith("archived-"):
+        sys.stderr.write("Passing of non archived PVC not allowed.")
+        sys.exit()
+
+    archived_pvs = get_archived_pvs(args.name, args.pvc)
+    if archived_pvs == -1:
+        sys.exit()
+
+    if archived_pvs:
+        sys.stdout.write("Found archived PVCs at storage pool %s\n" % args.name)
+        delete_archived_pvs(args.name, archived_pvs)
+        sys.stdout.write("Completed deletion of archived pvc(s) of storage-pool %s\n" %args.name)
+    else:
+        sys.stderr.write("No archived PVCs found at storage-pool %s" % args.name)
+
+
+if __name__ == "__main__":
+    main()
