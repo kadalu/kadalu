d6b1e7843664e0dc73685a88b9c85b07e0fd9958
Leela Gangavarapu <leelavg@thoughtexpo.com>
2022-05-05 12:39:24 +0530

    add monitoring label to kadalu ns for OCP (#813)
    fixes: #808
    ref: #809
    credits: @hunter86bg
    
    Signed-off-by: Leela Venkaiah G <leelavg@thoughtexpo.com>

--- tests/minikube.sh
+++ tests/minikube.sh
@@ -282,7 +282,7 @@ function validate_helm() {
         # Helm follows a specific order while installing/uninstalling (https://github.com/helm/helm/blob/release-3.0/pkg/releaseutil/kind_sorter.go#L27)
         # resources and it doesn't contain OpenShift 'SecurityContextConstraints' kind, so need to sort lines before 'diff'
         diff <(helm template --namespace kadalu helm/kadalu --set operator.enabled=true --set-string operator.kubernetesDistro=$distro,operator.verbose=$verbose | grep -v '#' | sort) \
-          <(grep -v '#' manifests/"$operator.yaml" | tail -n +6 | sed '/^kind: CustomResourceDefinition/,/^spec:/{/namespace/d}' | sort) --ignore-blank-lines
+          <(grep -v '#' manifests/"$operator.yaml" | tail -n +8 | sed '/^kind: CustomResourceDefinition/,/^spec:/{/namespace/d}' | sort) --ignore-blank-lines
       else
         diff <(helm template --namespace kadalu helm/kadalu --set operator.enabled=true --set-string operator.kubernetesDistro=$distro,operator.verbose=$verbose | grep -v '#') \
           <(grep -v '#' manifests/"$operator.yaml" | tail -n +6 | sed '/^kind: CustomResourceDefinition/,/^spec:/{/namespace/d}') --ignore-blank-lines
