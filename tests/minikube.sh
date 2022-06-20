0ee05446f5e9aff5ad0f55ee605e2e7b8e7b8ee0
Leela Venkaiah G <leelavg@thoughtexpo.com>
2022-06-20 13:45:22 +0530

    use helm for generating user deployable manifests
    - distinction b/n user and operator deployable resources/manifests
    - manifests generated by helm are placed in `manifests/` folder
    - remove reduntant files which are no longer needed
    
    fixes: #850
    
    Signed-off-by: Leela Venkaiah G <leelavg@thoughtexpo.com>

--- tests/minikube.sh
+++ tests/minikube.sh
@@ -116,6 +116,8 @@ function get_pvc_and_check() {
   echo Expanding PVCs from $log_text pool type
   sed "s/$original/$final/g" ${yaml_file} | kubectl apply -f -
 
+  # wait for pods to restart
+  sleep 60
   end_time=$(($(date +%s) + $time_limit))
   while [[ $($k get pod -l type=${label} -ojsonpath={'.items[].status.phase'} | grep -cv Succeeded) -ne 0 ]]; do
     [[ $end_time -lt $(date +%s) ]] && echo Sample pods for pool type "${log_text}" are not in complete state within ${time_limit}s after PVC expand && fail=1 && return
@@ -298,50 +300,17 @@ function display_metrics() {
 
 }
 
-function validate_helm() {
-
-  if [[ ! "$COMMIT_MSG" =~ 'helm skip' ]]; then
-
-    # As per https://github.com/actions/virtual-environments/blob/main/images/linux/Ubuntu2004-README.md
-    # `helm` is already part of github runner
-    for distro in kubernetes rke microk8s openshift; do
-      if [ "$distro" != "kubernetes" ]; then
-        export operator="kadalu-operator-$distro"
-        export nodeplugin="csi-nodeplugin-$distro"
-      else
-        export operator="kadalu-operator"
-        export nodeplugin="csi-nodeplugin"
-      fi
-      export verbose="yes" dist=$distro
-      echo Validating helm template for "'$distro'" against "'$operator'" [Empty for no diff]
-      echo
-
-      # Helm templates will not have 'kind: Namespace' so need to skip first 6 lines from operator manifest
-      if [ "$distro" == "openshift" ]; then
-        # Helm follows a specific order while installing/uninstalling (https://github.com/helm/helm/blob/release-3.0/pkg/releaseutil/kind_sorter.go#L27)
-        # resources and it doesn't contain OpenShift 'SecurityContextConstraints' kind, so need to sort lines before 'diff'
-        diff <(helm template --namespace kadalu helm/kadalu --set operator.enabled=true --set-string operator.kubernetesDistro=$distro,operator.verbose=$verbose | grep -v '#' | sort) \
-          <(grep -v '#' manifests/"$operator.yaml" | tail -n +8 | sed '/^kind: CustomResourceDefinition/,/^spec:/{/namespace/d}' | sort) --ignore-blank-lines
-      else
-        diff <(helm template --namespace kadalu helm/kadalu --set operator.enabled=true --set-string operator.kubernetesDistro=$distro,operator.verbose=$verbose | grep -v '#') \
-          <(grep -v '#' manifests/"$operator.yaml" | tail -n +6 | sed '/^kind: CustomResourceDefinition/,/^spec:/{/namespace/d}') --ignore-blank-lines
-      fi
-
-      echo Validating helm template for "'$distro'" against "'$nodeplugin'" [Empty for no diff]
-      echo
-      diff <(helm template --namespace kadalu helm/kadalu --set csi-nodeplugin.enabled=true --set-string csi-nodeplugin.kubernetesDistro=$distro,csi-nodeplugin.verbose=$verbose | grep -v '#') \
-        <(grep -v '#' manifests/"$nodeplugin.yaml") --ignore-blank-lines
-    done
-    unset operator nodeplugin verbose dist
-
-  fi
-}
-
 function deploy_kadalu_resources() {
   echo "Deploying kadalu operator and csi driver"
 
   # pick the operator file from repo
   sed -i -e 's/imagePullPolicy: Always/imagePullPolicy: IfNotPresent/g' manifests/kadalu-operator.yaml
+
+  # set verbose field
+  # TODO: Use helm values file
+  sed -i -e 's/"no"/"yes"/g' manifests/kadalu-operator.yaml
+  sed -i -e 's/"no"/"yes"/g' manifests/csi-nodeplugin.yaml
+
   kubectl apply -f manifests/kadalu-operator.yaml
   kubectl apply -f manifests/csi-nodeplugin.yaml
 
@@ -459,9 +428,6 @@ case "${1:-}" in
 
     # list docker images
     docker images
-
-    # validates helm templates against jinja generated manifests
-    validate_helm
 
     # deploys kadalu operator, csi driver and storage pools
     deploy_kadalu_resources
