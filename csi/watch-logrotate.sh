317a608ed3890d251cbb2d63e187f143a1ef497c
Shree Vatsa N <vatsa@kadalu.tech>
2022-11-14 17:07:55 +0530

    csi: Implement logrotate to free-up space used by logs
    This PR implements logrotate into kadalu's CSI POD to free-up space used by logs
    periodically.
    The logs will be rotated when minsize of 100KB, maxsize of 10MB will be reached.
    Atmost of 10 log archives will be available in loggig directory.
    The logrotate executable is triggered by watch-logrotate.sh process which will execute it
    every 8 hours.
    Signed-off-by: Shree Vatsa N <vatsa@kadalu.tech>

---
+++ csi/watch-logrotate.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+while true
+do
+  if [[ $(ls /var/log/gluster/*.gz | wc -l) -ge 9 ]]; then
+    rm -rf /var/log/gluster/*.gz
+  fi
+  logrotate /kadalu/logrotate.conf
+  /usr/bin/killall -HUP glusterfs > /dev/null 2>&1 || true
+  # Run logrotate for every 8 hours
+  sleep 28800
+done
