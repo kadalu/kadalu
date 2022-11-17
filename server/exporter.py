66ce8ee53ea8b7b2fb9cef09b27eaaaf2ad388d7
hunter86bg <hunter86_bg@yahoo.com>
2022-11-17 22:47:51 +0200

    Add metric support for cgroup2
    Signed-off-by: hunter86bg <hunter86_bg@yahoo.com>

--- server/exporter.py
+++ server/exporter.py
@@ -1,5 +1,6 @@
 import logging
 import os
+import re
 
 import uvicorn
 from fastapi import FastAPI
@@ -24,15 +25,27 @@ def metrics():
     memory_usage_in_bytes = -1
     cpu_usage_in_nanoseconds = -1
 
-    memory_usage_file_path = '/sys/fs/cgroup/memory/memory.usage_in_bytes'
+    if os.path.exists('/sys/fs/cgroup/cgroup.controllers'):  # If exists -> cgroup2
+        memory_usage_file_path = '/sys/fs/cgroup/system.slice/memory.current'
+        cpu_usage_file_path = '/sys/fs/cgroup/cpu.stat'
+    else:
+       memory_usage_file_path = '/sys/fs/cgroup/memory/memory.usage_in_bytes'
+       cpu_usage_file_path = '/sys/fs/cgroup/cpu/cpuacct.usage'
+
     if os.path.exists(memory_usage_file_path):
         with open(memory_usage_file_path, 'r') as memory_fd:
             memory_usage_in_bytes = int(memory_fd.read().strip())
 
-    cpu_usage_file_path = '/sys/fs/cgroup/cpu/cpuacct.usage'
-    if os.path.exists(cpu_usage_file_path):
-        with open(cpu_usage_file_path, 'r') as cpu_fd:
-            cpu_usage_in_nanoseconds = int(cpu_fd.read().strip())
+    if os.path.exists('/sys/fs/cgroup/cgroup.controllers'):  # If exists -> cgroup2
+        if os.path.exists(cpu_usage_file_path):
+            with open(cpu_usage_file_path, 'r') as cpu_fd:
+                for line in cpu_fd: # loop over all lines till we find usage_usec
+                    if re.search('usage_usec', line):
+                        cpu_usage_in_nanoseconds = int(line.split(' ')[1]) * 1000 # microseconds to nanoseconds
+    else:
+        if os.path.exists(cpu_usage_file_path):
+            with open(cpu_usage_file_path, 'r') as cpu_fd:
+                cpu_usage_in_nanoseconds = int(cpu_fd.read().strip())
 
     data["pod"] = {
         "memory_usage_in_bytes": memory_usage_in_bytes,
